70c13fb211bee58cc06ff30bcfbd16142af300cb
alo40 <alejandronieto40@gmail.com>
2023-09-02 23:27:46 +0200

    M1-12 correct f-string

--- Module_1-12.py
+++ Module_1-12.py
@@ -4,4 +4,4 @@ n = 31000
 k = 63
 p = k / n
 f = comb(n, k) * (p ** k) * (1 - p) ** (n - k)
-print(f"Probability of {63} ocurrences in {n} cases is {f}")
+print(f"Probability of {k} ocurrences in {n} cases is {f}")
