61d9e47b2cb651f15fedf9eb20cefb9db5fd8603
alo40 <alejandronieto40@gmail.com>
2023-09-02 23:56:41 +0200

    M1-12 add plotting

--- Module_1-12.py
+++ Module_1-12.py
@@ -1,7 +1,27 @@
+import numpy as np
 from scipy.special import comb
+import matplotlib.pyplot as plt
 
+
+def calculate_binomial_probability(n, k, p):
+    return comb(n, k) * (p ** k) * (1 - p) ** (n - k)
+
+
+# Single case
 n = 31000
 k = 63
 p = k / n
 f = comb(n, k) * (p ** k) * (1 - p) ** (n - k)
 print(f"Probability of {k} ocurrences in {n} cases is {f}")
+
+# Loop case
+N = k + 50
+pmf = np.zeros(N, dtype=float)
+for i in range(0, N):
+    pmf[i] = calculate_binomial_probability(n, i, p)
+    # print(pmf[i])
+
+# Plotting
+plt.plot(pmf)
+plt.grid()
+plt.show()
