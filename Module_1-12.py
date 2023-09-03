ce9236a4aca8f70d2c5d1a17794c52e142dead01
alo40 <alejandronieto40@gmail.com>
2023-09-03 13:45:19 +0200

    M1-12 correct small issues

--- Module_1-12.py
+++ Module_1-12.py
@@ -21,7 +21,7 @@ L = p * n  # lambda
 N = k + 50  # total loop iterations
 
 # Single Binomial case
-f = comb(n, k) * (p ** k) * (1 - p) ** (n - k)
+f = calculate_binomial_probability(n, k, p)
 
 # Binomial case
 pmf_binomial = np.zeros(N, dtype=float)
@@ -34,13 +34,13 @@ for i in range(0, N):
     pmf_poisson[i] = calculate_poisson_probability(L, i)
 
 # Numerical Integration
-significance_index = 63
+significance_index = 39
 dx = 1  # spacing of integration points, default is 1
 area_under_curve_simpson = simpson(pmf_poisson[:significance_index], dx=dx)
 print(f"The area under the curve using simpson is {area_under_curve_simpson}")
-
-area_under_curve_trapz = np.trapz(pmf_poisson[:significance_index], dx=dx)
-print(f"The area under the curve using trapz is {area_under_curve_trapz}")
+#
+# area_under_curve_trapz = np.trapz(pmf_poisson[:significance_index], dx=dx)  # not used
+# print(f"The area under the curve using trapz is {area_under_curve_trapz}")
 
 # Plotting
 plt.plot(pmf_binomial, label="Binomial")
@@ -49,7 +49,7 @@ plt.plot([significance_index, significance_index], [0, 0.05], color='red', lines
 plt.xlim([0, N])
 plt.legend()
 plt.grid()
-plt.title(f"Comparison Binomial vs Poisson distributions | n = {n} | k = {k} | p = {p:.4} | lambda = {L:.4} | area link to sig. line = {area_under_curve_simpson:.4}")
+plt.title(f"Comparison Binomial vs Poisson distributions | n = {n} | k = {k} | p = {p:.4} | lambda = {L:.4} | area left to sig. line = {area_under_curve_simpson:.4}")
 # plt.title(f"Probability of {k} ocurrences in {n} cases is {f:.4}")  # not used
 plt.xlabel("Number of patients")
 plt.ylabel("Probability of ocurrence")
