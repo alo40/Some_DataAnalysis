3f3ebc0ccee9d86c7952f6b9baf6476b124bef22
alo40 <alejandronieto40@gmail.com>
2023-09-21 07:43:16 +0200

    update Code_M2_L1-9_PrincipalComponentAnalysis.py

--- Code_M2_L1-9_PrincipalComponentAnalysis.py
+++ Code_M2_L1-9_PrincipalComponentAnalysis.py
@@ -15,12 +15,18 @@ from myLibrary import (calculate_sample_cov_matrix,
 # x1 = np.array([1, 1/2])
 # x2 = np.array([-1, -1/2])
 
-# exercise 11
-x1 = np.array([0, 2])
-x2 = np.array([1, -1])
-x3 = np.array([-1, -1])
+# # exercise 11
+# x1 = np.array([0, 2])
+# x2 = np.array([1, -1])
+# x3 = np.array([-1, -1])
 
-x = np.array([x1, x2, x3])
+# exercise 12
+x1 = np.array([0, 2])
+x2 = np.array([0, -2])
+x3 = np.array([1, 1])
+x4 = np.array([-1, -1])
+
+x = np.array([x1, x2, x3, x4])
 S = calculate_sample_cov_matrix(x)
 # n = x.shape[0]
 # S = 1 / n * x.T @ x
@@ -36,16 +42,19 @@ v2 = eigenvectors[:, 0]
 p1 = calculate_projection_vector(x1, v1)
 p2 = calculate_projection_vector(x2, v1)
 p3 = calculate_projection_vector(x3, v1)
+p3 = calculate_projection_vector(x4, v1)
 
 y1 = calculate_projection_scalar(x1, v1)
 y2 = calculate_projection_scalar(x2, v1)
 y3 = calculate_projection_scalar(x3, v1)
-y = np.array([y1, y2, y3])
+y4 = calculate_projection_scalar(x4, v1)
+y = np.array([y1, y2, y3, y4])
 sigma_y = calculate_sample_variance(y)
 
 print(f"signed length projection of x1 onto v1 is {y1}")
 print(f"signed length projection of x2 onto v1 is {y2}")
-print(f"signed length projection of x3 onto v1 is {y2}")
+print(f"signed length projection of x3 onto v1 is {y3}")
+print(f"signed length projection of x3 onto v1 is {y4}")
 print(f"sample variance of projected data is {sigma_y}")
 
 fig = plt.figure()
@@ -54,17 +63,23 @@ ax = fig.add_subplot()
 ax.plot([0, x1[0]], [0, x1[1]], marker='o', label=f"vector x1")
 ax.plot([0, x2[0]], [0, x2[1]], marker='o', label=f"vector x2")
 ax.plot([0, x3[0]], [0, x3[1]], marker='o', label=f"vector x3")
+ax.plot([0, x4[0]], [0, x4[1]], marker='o', label=f"vector x4")
 
 ax.plot([0, v1[0]], [0, v1[1]], marker='v', label=f"eigenvector v1")
 ax.plot([0, v2[0]], [0, v2[1]], marker='v', label=f"eigenvector v2")
 
-m1 = y1*v1
-ax.plot(m1[0], m1[1], marker='x', markersize=10, label=f"projection of x1 in v1")
-m2 = y2*v1
-ax.plot(m2[0], m2[1], marker='x', markersize=10, label=f"projection of x2 in v1")
-m3 = y3*v1
-ax.plot(m3[0], m3[1], marker='x', markersize=10, label=f"projection of x3 in v1")
-
+x1_proj_v1 = calculate_projection_scalar(x1, v1)
+x1_proj_v2 = calculate_projection_scalar(x1, v2)
+ax.plot(x1_proj_v1, x1_proj_v2, marker='x', markersize=10, label=f"projection of x1")
+x2_proj_v1 = calculate_projection_scalar(x2, v1)
+x2_proj_v2 = calculate_projection_scalar(x2, v2)
+ax.plot(x2_proj_v1, x2_proj_v2, marker='x', markersize=10, label=f"projection of x2")
+x3_proj_v1 = calculate_projection_scalar(x3, v1)
+x3_proj_v2 = calculate_projection_scalar(x3, v2)
+ax.plot(x3_proj_v1, x3_proj_v2, marker='x', markersize=10, label=f"projection of x3")
+x4_proj_v1 = calculate_projection_scalar(x4, v1)
+x4_proj_v2 = calculate_projection_scalar(x4, v2)
+ax.plot(x4_proj_v1, x4_proj_v2, marker='x', markersize=10, label=f"projection of x4")
 
 ax.set_xlabel('X-Axis')
 ax.set_ylabel('Y-Axis')
