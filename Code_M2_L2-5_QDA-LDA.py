60cc0558657322037aef53fb794a7a049a579e25
alo40 <alejandronieto40@gmail.com>
2023-09-24 13:48:53 +0200

    update Code_M2_L2-5_QDA-LDA.py

--- Code_M2_L2-5_QDA-LDA.py
+++ Code_M2_L2-5_QDA-LDA.py
@@ -3,7 +3,7 @@ from numpy.linalg import inv
 
 mu0 = np.array([0, 0])
 mu1 = np.array([1, 0])
-A = np.array([[1, 0], [0, 1]])
+A = np.array([[2, 0], [0, 1]])
 
 v0 = 1 / np.sqrt(2) * np.array([1,  1])
 v1 = 1 / np.sqrt(2) * np.array([1, -1])
@@ -11,4 +11,8 @@ v = np.array([v0, v1]).T
 
 sigma = v @ A @ v.T
 normal = ((mu0 - mu1).T @ inv(sigma)).T
-pass
+
+P = v @ np.sqrt(inv(A))
+print(f"P = \n{P}")
+I = P.T @ sigma @ P
+print(f"I = \n{I}")
