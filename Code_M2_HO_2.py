decc6d21f845cad730ed9971e20436a1d8e6ed4f
alo40 <alejandronieto40@gmail.com>
2023-09-28 19:41:26 +0200

    add Code_M2_HO_wineExample.py

--- Code_M2_HO_2.py
+++ Code_M2_HO_2.py
@@ -6,6 +6,9 @@ from sklearn.manifold import MDS
 from sklearn.manifold import TSNE
 # import pandas as pd
 # import polars as pl
+from sklearn.cluster import KMeans
+# from sklearn.metrics import silhouette_score, silhouette_samples
+# from yellowbrick.style.colors import resolve_colors
 
 # # transform npy to csv (use only once)
 # X = np.load("data/p1/X.npy")
@@ -17,13 +20,75 @@ from sklearn.manifold import TSNE
 X = np.load("data/p1/X.npy")
 X = np.log2(X + 1)
 
+# # k-means colors
+# kmeans = KMeans(n_clusters=50, n_init=10)
+# y = kmeans.fit_predict(X)
+
+# PCA
+pca = PCA(n_components=2)
+pca.fit(X)
+
+kmeans = KMeans(n_clusters=4, n_init=10)
+y = kmeans.fit_predict(X)
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 3))
+ax.scatter(X.dot(pca.components_[0]),  X.dot(pca.components_[1]), c=y)
+ax.set(xlabel="Projected data onto first PCA component", ylabel="y")
+plt.tight_layout()
+
+# plt.plot(np.arange(2, 5), [silhouette_score(X, KMeans(i, n_init=50).fit(X).labels_) for i in range(2, 5)])
+plt.show()
+
+
+# # MDS visualization
+# embedding = MDS(n_components=2, normalized_stress='auto')
+# X_transformed = embedding.fit_transform(X)
+# X_transformed.shape
+# plt.scatter(X_transformed[:, 0], X_transformed[:, 1], c=y)
+# plt.show()
+
+
+# # TSNE visualization
+# pca = PCA()  # Initialize with n_components parameter to only find the top eigenvectors
+# z = pca.fit_transform(X)
+# tsne = TSNE(n_components=2, perplexity=100, verbose=1)
+# X_embedded = tsne.fit_transform(z[:, 0:50])
+#
+# kmeans = KMeans(n_clusters=4, n_init=10)
+# y = kmeans.fit_predict(X)
+#
+# plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=y)
+# plt.axis("equal")
+#
+# # plt.plot(np.arange(2,10),
+# #          [silhouette_score(X_embedded,KMeans(i,n_init=50).fit(X_embedded).labels_) for i in range(2,10)])
+#
+# plt.show()
+
+
+# ###########################################################################################
+# # not used
+# print(f"var cumulative sum = {suma}")
+# print(f"index where cumsum > 0.85 is {np.where(suma > 0.85)[0][0]}")
+
+# # elbow plot using k-means
+# n = 10
+# all_kmeans = [KMeans(n_clusters=i+1, n_init=10) for i in range(n)]
+# for i in range(n):
+#     all_kmeans[i].fit(X_embedded)
+# intertias = [all_kmeans[i].inertia_ for i in range(n)]
+# plt.title("KMeans Sum of Squares Criterion")
+# plt.xlabel("# cluster")
+# plt.ylabel("within-cluster sum of squares")
+# plt.plot(intertias)
+# plt.xlim(0, n)
+# plt.xticks(np.arange(0, n, step=1))
+# plt.grid()
+# plt.show()
+
 # # PCA
-# pca = PCA(n_components=2)
-# pca.fit(X)
 # var = pca.explained_variance_ratio_
 # suma = var.cumsum()
-# # print(f"var cumulative sum = {suma}")
-# print(f"index where cumsum > 0.85 is {np.where(suma > 0.85)[0][0]}")
 
 # # plotting without visualization technique
 # plt.scatter(X[:, 0], X[:, 1], alpha=0.3, label="samples")
@@ -44,22 +109,3 @@ X = np.log2(X + 1)
 # )
 # plt.legend()
 # plt.show()
-
-# # PCA visualization (2 principal components)
-# fig, ax = plt.subplots(1, 1, figsize=(10, 3))
-# ax.scatter(X.dot(pca.components_[0]),  X.dot(pca.components_[1]), alpha=0.3)
-# ax.set(xlabel="Projected data onto first PCA component", ylabel="y")
-# plt.tight_layout()
-# plt.show()
-
-# # MDS visualization
-# embedding = MDS(n_components=2, normalized_stress='auto')
-# X_transformed = embedding.fit_transform(X)
-# X_transformed.shape
-# plt.scatter(X_transformed[:, 0], X_transformed[:, 1])
-# plt.show()
-
-# TSNE visualization
-X_embedded = TSNE(n_components=50, learning_rate='auto',init='random', perplexity=40).fit_transform(X)
-plt.scatter(X_embedded[:, 0], X_embedded[:, 1])
-plt.show()
---
+++ Code_M2_HO_wineExample.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_wine
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+
+features, target = load_wine(return_X_y=True)
+features.shape
+wine_std = (features-np.mean(features,0))/np.std(features,0)
+
+# PCA
+# pca_wine = PCA(5).fit(wine_std)
+# pcs = pca_wine.transform(wine_std)
+
+X = np.load("data/p1/X.npy")
+X = np.log2(X + 1)
+pca = PCA(2).fit(X)
+pcs = pca.transform(X)
+
+# plt.scatter(pcs[:,0],pcs[:,1])
+# plt.title("Wine Data PCs",size=18)
+# plt.xlabel("PC 1",size=14)
+# plt.ylabel("PC 2",size=14)
+# plt.axis("equal")
+# plt.show()
+#
+# # MDS
+# mds_wine = MDS(2).fit_transform(wine_std)
+# plt.scatter(mds_wine[:,0],mds_wine[:,1])
+# plt.title("Wine Data MDS",size=18)
+# plt.axis("equal")
+# plt.show()
+
+# TSNE perpexitly loop
+# for perplexity in [5,10,30,50,80,100]:
+#     tsne_wine = TSNE(n_components=2,perplexity=perplexity).fit_transform(wine_std)
+#     plt.scatter(tsne_wine[:,0],tsne_wine[:,1])
+#     plt.title("Wine Data TSNE, perplexity "+str(perplexity),size=18)
+#     plt.axis("equal")
+#     plt.show()
+
+# # TSNE
+# tsne_wine = TSNE(n_components=2,perplexity=50).fit_transform(wine_std)
+# plt.scatter(tsne_wine[:,0],tsne_wine[:,1])
+# plt.title("Wine Data TSNE, perplexity 50",size=18)
+# plt.axis("equal")
+# plt.show()
+
+# # elbow plot
+# plt.plot(np.arange(1,6),[KMeans(i,n_init=50).fit(wine_std).inertia_ for i in range(1,6)])
+# plt.title("KMeans Sum of Squares Criterion",size=18)
+# plt.xlabel("# Clusters",size=14)
+# plt.ylabel("Within-Cluster Sum of Squares",size=14)
+# plt.show()
+
+# # silhouette plot
+# plt.plot(np.arange(2,6),[silhouette_score(wine_std,KMeans(i,n_init=50).fit(wine_std).labels_) for i in range(2,6)])
+# plt.title("Average Silhouette Scores",size=18)
+# plt.xlabel("# Clusters",size=14)
+# plt.ylabel("Average Silhouette Score",size=14)
+# plt.show()
+
+
+from sklearn.metrics import silhouette_score, silhouette_samples
+from yellowbrick.style.colors import resolve_colors
+from yellowbrick.cluster import SilhouetteVisualizer
+
+# Horizontal axes on the plots are not all the same range, be careful
+# for i in range(2, 6):
+
+i = 6
+clustering = KMeans(i, n_init=50)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+visualizer = SilhouetteVisualizer(clustering, colors='yellowbrick', is_fitted=False, ax=ax1)
+visualizer.fit(X)
+
+colors = np.array(resolve_colors(i, "yellowbrick"))
+ax2.scatter(pcs[:, 0], pcs[:, 1], c=colors[clustering.labels_])
+ax2.axis("equal")
+
+# If we want to set axes to be the same for all plots, need to do something like this
+# instead of visualizer.show(), which resets the axes
+visualizer.finalize()
+ax1.set_xlim((-.2, .6))
+plt.show()
