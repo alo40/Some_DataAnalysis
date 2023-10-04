c81d6be816098e881b098126a8f7a95f12bedbc6
alo40 <alejandronieto40@gmail.com>
2023-10-04 23:52:22 +0200

    update Code_M2_HO-5_LogisticRegression.py

--- Code_M2_HO-5_LogisticRegression.py
+++ Code_M2_HO-5_LogisticRegression.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from tqdm import trange
 from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from myLibrary import calculate_sample_cov_matrix
 
 
 # # -----------------------------------------------------------------
@@ -81,45 +83,45 @@ from sklearn.manifold import TSNE
 # # np.save('data/p2_unsupervised_reduced/model_coefficients.npy', model.coef_)  # do it only once!
 
 
-# # -----------------------------------------------------------------
-# # regression model using top-100 selected features
-#
-# # find top 100 features
-# model_coef = np.load("data/p2_unsupervised_reduced/model_coefficients.npy")
-# sum_column = np.abs(model_coef).sum(axis=0)
-# top_features = np.argsort(np.abs(sum_column))[::-1][:100]
-#
-# # load evaluation train and test data
-# x_train = np.load("data/p2_evaluation_reduced/X_train.npy")
-# x_test = np.load("data/p2_evaluation_reduced/X_test.npy")
-# y_train = np.load("data/p2_evaluation_reduced/y_train.npy")
-# y_test = np.load("data/p2_evaluation_reduced/y_test.npy")
-#
-# # use selected features
-# x_train_selection = x_train[:, top_features]
-# x_test_selection = x_test[:, top_features]
-#
-# # train model
-# model = LogisticRegression(solver='liblinear', C=0.05, multi_class='ovr', penalty='l2')
-# model.fit(x_train_selection, y_train)
-#
-# y_pred = model.predict(x_test_selection)
-#
-# model.score(x_train_selection, y_train)
-# model.score(x_test_selection, y_test)
-#
-# # confusion_matrix(y_test, y_pred)
-# # print(classification_report(y_test, y_pred))
-
-
 # -----------------------------------------------------------------
-# regression model using random selected features
+# regression model using top-100 selected features
+
+# find 100 features with highest coefficients
+model_coef = np.load("data/p2_unsupervised_reduced/model_coefficients.npy")
+sum_column = np.abs(model_coef).sum(axis=0)
+coef_features = np.argsort(np.abs(sum_column))[::-1][:100]
 
 # load evaluation train and test data
 x_train = np.load("data/p2_evaluation_reduced/X_train.npy")
 x_test = np.load("data/p2_evaluation_reduced/X_test.npy")
 y_train = np.load("data/p2_evaluation_reduced/y_train.npy")
 y_test = np.load("data/p2_evaluation_reduced/y_test.npy")
+
+# use selected features
+x_train_selection = x_train[:, coef_features]
+x_test_selection = x_test[:, coef_features]
+
+# train model
+model = LogisticRegression(solver='liblinear', C=0.05, multi_class='ovr', penalty='l2')
+model.fit(x_train_selection, y_train)
+
+y_pred = model.predict(x_test_selection)
+
+print(f"model score train data (coefficient case): {model.score(x_train_selection, y_train)}")
+print(f"model score test data (coefficient case): {model.score(x_test_selection, y_test)}\n")
+
+# confusion_matrix(y_test, y_pred)
+# print(classification_report(y_test, y_pred))
+
+
+# -----------------------------------------------------------------
+# regression model using random selected features
+
+# # load evaluation train and test data
+# x_train = np.load("data/p2_evaluation_reduced/X_train.npy")
+# x_test = np.load("data/p2_evaluation_reduced/X_test.npy")
+# y_train = np.load("data/p2_evaluation_reduced/y_train.npy")
+# y_test = np.load("data/p2_evaluation_reduced/y_test.npy")
 
 # find 100 random features
 pool_of_numbers = np.arange(1, 20001)  # Numbers from 1 to 20,000
@@ -135,11 +137,51 @@ model.fit(x_train_selection, y_train)
 
 y_pred = model.predict(x_test_selection)
 
-print(f"model score train data: {model.score(x_train_selection, y_train)}")
-print(f"model score train data: {model.score(x_test_selection, y_test)}")
+print(f"model score train data (random case): {model.score(x_train_selection, y_train)}")
+print(f"model score test data (random case): {model.score(x_test_selection, y_test)}\n")
+pass
+# confusion_matrix(y_test, y_pred)
+# print(classification_report(y_test, y_pred))
+
+
+# -----------------------------------------------------------------
+# regression model using high-variance selected features
+
+# # load evaluation train and test data
+# x_train = np.load("data/p2_evaluation_reduced/X_train.npy")
+# x_test = np.load("data/p2_evaluation_reduced/X_test.npy")
+# y_train = np.load("data/p2_evaluation_reduced/y_train.npy")
+# y_test = np.load("data/p2_evaluation_reduced/y_test.npy")
+
+# find 100 features with highest variance
+S = calculate_sample_cov_matrix(x_train)  # covariance matrix
+S_diag = np.diag(S)  # features variances
+variance_features = np.argsort(S_diag)[::-1][:100]
+
+# use selected features
+x_train_selection = x_train[:, variance_features]
+x_test_selection = x_test[:, variance_features]
+
+# train model
+model = LogisticRegression(solver='liblinear', C=0.05, multi_class='ovr', penalty='l2')
+model.fit(x_train_selection, y_train)
+
+y_pred = model.predict(x_test_selection)
+
+print(f"model score train data (variance case): {model.score(x_train_selection, y_train)}")
+print(f"model score test data (variance case): {model.score(x_test_selection, y_test)}\n")
 
 # confusion_matrix(y_test, y_pred)
 # print(classification_report(y_test, y_pred))
+
+# plot histograms
+plt.hist(S_diag[variance_features], bins=100, label="100 highest variances")
+plt.hist(S_diag[coef_features], bins=10, label="100 highest coefficients")
+plt.title("Comparison 100 features with highest coefficients vs 100 features with highest variances ")
+# plt.xlabel("Number of Components", size=14)
+# plt.ylabel("% Variance Explained", size=14)
+plt.legend()
+plt.show()
 
 
 # # -----------------------------------------------------------------
