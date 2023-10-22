5a595af0ca529dde0a59fbdca9ad5d41b325ca4b
alo40 <alejandronieto40@gmail.com>
2023-10-23 00:43:39 +0200

    update Code_M3_L4-3_Erdos-RenyiModel.py

--- Code_M3_L4-3_Erdos-RenyiModel.py
+++ Code_M3_L4-3_Erdos-RenyiModel.py
@@ -12,10 +12,22 @@ from myLibrary import calculate_binomial_probability
 # # print(n * (n - 1) / 2)
 # print(calculate_binomial_probability(n, k, p))
 
-# Erdos-Renyi graph
-n = 20
-p = 0.14
-G = nx.erdos_renyi_graph(n, p, seed=None, directed=False)
-pos = nx.kamada_kawai_layout(G)
-nx.draw_networkx(G, pos, labels={i: str(i + 1) for i in range(n)})
-plt.show()
+# # Erdos-Renyi graph
+# n = 20
+# p = 0.14
+# G = nx.erdos_renyi_graph(n, p, seed=None, directed=False)
+# pos = nx.kamada_kawai_layout(G)
+# nx.draw_networkx(G, pos, labels={i: str(i + 1) for i in range(n)})
+# plt.show()
+
+# Bibliographic coupling & Cocitation
+# A = np.array([[0, 1, 1], [0, 0, 0], [0, 0, 0]])
+# A = np.array([[0, 0, 1, 0, 0], [0, 0, 0, 1, 1], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+A = np.array([[0, 1, 0, 1, 0], [0, 0, 1, 0, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0], [0, 1, 0, 1, 0]])
+G = nx.from_numpy_array(np.array(A), create_using=nx.DiGraph)
+B = A.T @ A  # bibliographic coupling
+C = A @ A.T  # cocitation
+# pos = nx.kamada_kawai_layout(G)
+# nx.draw_networkx(G, pos, labels={i: str(i + 1) for i in range(len(A))})
+# plt.show()
+pass
