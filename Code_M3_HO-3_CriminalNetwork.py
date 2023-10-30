e0689ca70d33b7217be7c9e7a86ab8b587f13ecf
alo40 <alejandronieto40@gmail.com>
2023-10-30 14:00:21 +0100

    update Code_M3_HO-3_CriminalNetwork.py

--- Code_M3_HO-3_CriminalNetwork.py
+++ Code_M3_HO-3_CriminalNetwork.py
@@ -17,15 +17,51 @@ for i in range(1, 12):
   G[i] = nx.from_pandas_adjacency(phases[i])
   G[i].name = var_name
 
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
-for i in range(1, 12):
+# line_nodes = np.array(range(1, 12+1))
+# line_edges = np.array(range(1, 12+1))
+# for i in range(1, 12):
+phases_list = [9, 10, 11]
+for i in phases_list:
   # print(f"phase {i}, eigenvector centrality = {nx.eigenvector_centrality(G[i])}\n")
   # print(f"phase {i}, betweenness centrality = {nx.betweenness_centrality(G[i], normalized=True)}\n")
   # print(f"phase {i}, degree centrality = {nx.degree_centrality(G[i])}\n")
-  # print(f"phase {i}, nodes = {G[i].number_of_nodes()}, edges = {G[i].number_of_edges()}\n")
-  nx.draw_networkx(G[i])
-  plt.title(f"phase {i}")
-  plt.show()
+  num_nodes = G[i].number_of_nodes()
+  num_edges = G[i].number_of_edges()
+  print(f"phase {i}, nodes = {num_nodes}, edges = {num_edges}\n")
+  nx.draw_networkx(G[i], ax=axs[i - phases_list[0]])
+  axs[i - phases_list[0]].set_title(f"phase {i}")
+fig.suptitle("Graphs of criminal network evolution")
+plt.show()
+  #
+#   line_nodes[i] = num_nodes
+#   line_edges[i] = num_edges
+# plt.plot(line_nodes, 'rx-', label='nodes')
+# plt.plot(line_edges, 'mo-', label='edges')
+# #
+# # seizure
+# n = 4
+# plt.plot([n, n], [0, 60], 'b--')
+# plt.text(n, 10, f'seizure phase {n}', rotation=90, ha='right', va='bottom')
+# #
+# # seizure
+# n = 6
+# plt.plot([n, n], [0, 60], 'b--')
+# plt.text(n, 10, f'seizure phase {n}', rotation=90, ha='right', va='bottom')
+# #
+# # seizure
+# n = 8
+# plt.plot([n, n], [0, 60], 'b--')
+# plt.text(n, 10, f'seizure phase {n}', rotation=90, ha='right', va='bottom')
+# #
+# plt.ylim(0, 60)
+# plt.xticks(np.arange(0, 12))
+# plt.legend()
+# plt.title('Evolution of a criminal network')
+# plt.ylabel('number of nodes/edges')
+# plt.xlabel('phases')
+# plt.show()
 
 
 # time_eingevector = np.zeros(12)
